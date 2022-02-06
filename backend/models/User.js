dfea55201717930e5413d59b52dd5b7b8e14cb0d
JabezSanjay <jbzsanjay2000@gmail.com>
2022-02-06 19:02:59 +0530

    Minor fix in user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  createedBy: {
+  createdBy: {
     type: mongoose.Schema.ObjectId,
     ref: 'User',
   },
